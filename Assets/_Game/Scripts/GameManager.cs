29b40c27c4b33f0febcb75b052ac45b8758eff3d
GiuseppeLozioEllena <giuseppe.ellena@studenti.unimi.it>
2017-11-04 14:29:22 +0100

    testPlayer
    setup player movement and test it

---
+++ Assets/_Game/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    // Use this for initialization
+    public static GameManager instance;
+    public Transform[] lanes;
+    private  Transform currentLane;
+    private int indexLane;
+    public GameObject player;
+    public Transform Target;
+	void Start ()
+    {
+         
+	}
+    private void Awake()
+    {
+        instance = this;
+        indexLane = 1;
+        currentLane = lanes[indexLane];
+
+    }
+
+    public void ShiftRightLane()
+    {
+        if (indexLane > 0)
+        {
+            indexLane--;
+            currentLane = lanes[indexLane];
+            player.transform.position=new Vector3(currentLane.position.x, player.transform.position.y, player.transform.position.z);
+        }
+    }
+
+    public void ShiftleftLane()
+    {
+        if (indexLane < lanes.Length-1)
+        {
+            indexLane++;
+            currentLane = lanes[indexLane];
+            player.transform.position=new Vector3(currentLane.position.x, player.transform.position.y, player.transform.position.z);
+        }
+    }
+
+    public Transform getCurrentLaneTarget()
+    {
+        return lanes[indexLane].GetChild(0).transform;
+    }
+    // Update is called once per frame
+    void Update () {
+
+        if (Vector3.Distance(player.transform.position,Target.position)<10f)
+            ResetPlayerPos();
+
+    }
+
+    void ResetPlayerPos()
+    {
+        player.transform.position = Vector3.zero;
+    }
+}
---
+++ Assets/_Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    
+    public float speed = 4f;
+    Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameManager.instance.getCurrentLaneTarget();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown("d"))
+        {
+
+            GameManager.instance.ShiftleftLane();
+            target = GameManager.instance.getCurrentLaneTarget();
+
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+
+            GameManager.instance.ShiftRightLane();
+            target = GameManager.instance.getCurrentLaneTarget();
+
+        }
+
+        transform.position= Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        speed += 0.001f;
+        
+    }
+
+
+}
